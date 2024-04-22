f6647fe61d77614f55bf56d04aa8aeca14db3650
Muhairwe144J <mnasasirashafik144j@gmail.com>
2024-04-22 23:50:13 +0300

    warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const argsLength = process.argv.length; // Get arguments excluding script name
 
 if (argsLength === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (argsLength === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
