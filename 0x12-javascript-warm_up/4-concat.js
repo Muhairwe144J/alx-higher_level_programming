02701ff7ad434cd95448205fd8191626b8c4a5ad
Muhairwe144J <mnasasirashafik144j@gmail.com>
2024-04-23 00:24:26 +0300

    warm up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const args = process.argv.slice(2); // Get arguments excluding script name
 
-if (args.length < 2) {
-  console.log('undefined is undefined');
-} else {
-  console.log(`${args[0]} is ${args[1]}`);
-}
+/* Get arguments excluding script name*/
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
