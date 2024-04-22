4bf7f240f3db5a410854fb43725f5ed11ac7ed33
Muhairwe144J <mnasasirashafik144j@gmail.com>
2024-04-23 00:38:26 +0300

    warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
+const arg = parseInt(process.argv[2]);
 
-if (process.argv[2] === undefined || is NaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+if (isNaN(arg)) {
+  console.log("Missing number of occurrences");
 } else {
-	const x = Number(process.argv[2]);
-	let i = 0; 
-	i < x) {
-    console.log("C is fun");
-		i++;
+  for (let i = 0; i < arg; i++) {
+    console.log('C is fun');
   }
 }
